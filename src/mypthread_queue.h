8a12d3815f69d1c244c367fa54e82628a574ac3c
Jose Godinez <rodolfojose1996@gmail.com>
2018-04-07 15:35:02 -0600

    iniciando de 0

--- src/mypthread_queue.h
+++
@@ -1,7 +0,0 @@
-
-extern void mythread_q_init(mythread_private_t *node);
-extern void mythread_q_add(mythread_private_t *node);
-extern void mythread_q_delete(mythread_private_t *node);
-extern void mythread_q_state_display();
-extern mythread_private_t * mythread_q_search(unsigned long );
-
