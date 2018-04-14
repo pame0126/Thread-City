161dcd1dd8d8817b16ed31922a259c45290c066d
pame0126 <pamelasofiaalejandra1996@gmail.com>
2018-04-13 20:41:26 -0600

    limpiar

--- src/mypthread_queue.h
+++
@@ -1,5 +0,0 @@
-extern void my_thread_q_init(my_thread_private_t *node);
-extern void my_thread_q_add(my_thread_private_t *node);
-extern void my_thread_q_delete(my_thread_private_t *node);
-extern void my_thread_q_state_display();
-extern my_thread_private_t * my_thread_q_search(unsigned long );
