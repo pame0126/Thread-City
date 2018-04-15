b1e1d22f61d5508f6810b85a8dce9fd414e9769f
pame0126 <pamelasofiaalejandra1996@gmail.com>
2018-04-14 20:40:40 -0600

    corregido

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include <mypthread.h>
+#include <mypthreads.h>
 
 #include <time.h>
 #include <ruta_moviles.h>
@@ -16,41 +16,17 @@
 #define NOT_RT		-1
 
 
-
-/*
- * aumenta el contador en 50,
- * cuando tiene el control de regreso le suma otros 50 y sale
- */
-void *thread_func(void *arg)
-{
-	int *count = (int *)arg;
-
-	*count = *count + 50;
-	LOG_PRINTF("Thread %ld: Incremented count by 50 and will now yield\n", (unsigned long)mythread_self().tid);
-	my_thread_yield();
-	*count = *count + 50;
-	LOG_PRINTF("Thread %ld: Incremented count by 50 and will now exit\n", (unsigned long)mythread_self().tid);
-	my_thread_end(NULL);
-	return NULL;
-}
-
-void*prints(void*arg){
-	printf("INICIO\n");
-	my_thread_yield();
-	my_thread_end(NULL);
-	return NULL;
-}
-
 void*pausa(void*arg){
-	my_thread_yield();
+	mythread_yield();
 	while(1){
 	sleep(1);
 	print_matriz();
-	my_thread_yield();
+	mythread_yield();
 	}
 	
 	return NULL;
 }
+
 /*
  * ejemplo de uso de la biblioteca
  */
@@ -65,23 +41,23 @@ int main(int argc, char *argv[])
 	//int count[NTHREADS];
 	int i;
 	char *status;
-	my_thread_init();
+	mythread_init();
 	//imprimir matriz
 	int x = 0;
 	for(; x < 4;x++){
-		my_thread_create(&threads[x], NULL, arrancar_carro, &((lis->list_carros)[x]), NOT_RT);
+		mythread_create(&threads[x], NULL, arrancar_carro, &((lis->list_carros)[x]), NOT_RT);
 	}
-	my_thread_create(&threads[x], NULL, control_semaforos, &((lis->list_carros)[x]), 1);x++;
-	my_thread_create(&threads[x], NULL, puente_un_carril, &((lis->list_carros)[x]), 1);x++;
-	my_thread_create(&threads[x], NULL, pausa, &((lis->list_carros)[x]), 1);
+	mythread_create(&threads[x], NULL, control_semaforos, &((lis->list_carros)[x]), 1);x++;
+	mythread_create(&threads[x], NULL, puente_un_carril, &((lis->list_carros)[x]), 1);x++;
+	mythread_create(&threads[x], NULL, pausa, &((lis->list_carros)[x]), 1);
 
-	my_thread_chsched(0);
+	mythread_chsched(0);
 	
 	for (i = 0; i < x; i++) {
 		//arreglar el error 
-		my_thread_join(threads[i], (void **)&status);
+		mythread_join(threads[i], (void **)&status);
 	}
-	my_thread_end(NULL);
+	mythread_end(NULL);
 	
 	return 0;
 }
